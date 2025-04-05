f9e053fe7cc4fef4296f495181c5dd57f64ef6e3
ViossaTV <contact@viossa.tv>
2025-04-05 02:30:48 +0100

    Update streetCard.tsx

--- src/components/ingame/streetCard.tsx
+++ src/components/ingame/streetCard.tsx
@@ -73,69 +73,6 @@ function StreetCard({ args }: { args: StreetDisplayInfo }) {
     return (
         <div className="street-card">
             <div style={{ backgroundColor: _color }}>
-                <p>title dreed</p>
-                <h3>{args.title}</h3>
-            </div>
-            <div>
-                <ol>
-                    <li>
-                        <p>Rent</p>
-                        <p>{args.rent}M</p>
-                    </li>
-                    <li>
-                        <p>Rent with color set</p>
-                        <p>{args.rentWithColorSet}M</p>
-                    </li>
-                    <li>
-                        <p>Rent with 1 house</p>
-                        <p>{args.multpliedrent[0]}M</p>
-                    </li>
-                    <li>
-                        <p>Rent with 2 houses</p>
-                        <p>{args.multpliedrent[1]}M</p>
-                    </li>
-                    <li>
-                        <p>Rent with 3 houses</p>
-                        <p>{args.multpliedrent[2]}M</p>
-                    </li>
-                    <li>
-                        <p>Rent with 4 houses</p>
-                        <p>{args.multpliedrent[3]}M</p>
-                    </li>
-                    <li>
-                        <p>Rent with hotel</p>
-                        <p>{args.multpliedrent[4]}M</p>
-                    </li>
-                </ol>
-                <hr />
-                <ol>
-                    <li>
-                        <p>House cost</p>
-                        <p>{args.housesCost}M each</p>
-                    </li>
-                    <li>
-                        <p>Hotels cost</p>
-                        <label>
-                            {args.hotelsCost}M each
-                            <br />
-                            <p style={{ fontSize: 12 }}>(plus 4 houses)</p>
-                        </label>
-                    </li>
-                </ol>
-
-                <br />
-                <hr />
-                <h4>{args.cardCost}M</h4>
-            </div>
-        </div>
-    );
-}
-
-function StreetCard({ args }: { args: StreetDisplayInfo }) {
-    const _color = translateGroup(args.group);
-    return (
-        <div className="street-card">
-            <div style={{ backgroundColor: _color }}>
                 <p>Lantharting</p>
                 <h3>{args.title}</h3>
             </div>
@@ -166,7 +103,7 @@ function StreetCard({ args }: { args: StreetDisplayInfo }) {
                         <p>{args.multpliedrent[3]} Ipni</p>
                     </li>
                     <li>
-                        <p>Rent med mbogohuomi</p>
+                        <p>Mbogo med mbogohuomi</p>
                         <p>{args.multpliedrent[4]} Ipni</p>
                     </li>
                 </ol>
@@ -189,6 +126,45 @@ function StreetCard({ args }: { args: StreetDisplayInfo }) {
                 <br />
                 <hr />
                 <h4>{args.cardCost} ipni</h4>
+            </div>
+        </div>
+    );
+}
+
+function RailroadCard({ args }: { args: RailroadDisplayInfo }) {
+    return (
+        <div className="street-card">
+            <div data-clear>
+                <img
+                    data-type="rail"
+                    src={RailIcon.replace("/public", "")}
+                    alt=""
+                />
+                <h3>{args.title}</h3>
+            </div>
+            <div>
+                <ol>
+                    <li>
+                        <p>Rent</p>
+                        <p>{25}M</p>
+                    </li>
+                    <li>
+                        <p>If 2 stations are owned</p>
+                        <p>{50}M</p>
+                    </li>
+                    <li>
+                        <p>If 3 stations are owned</p>
+                        <p>{100}M</p>
+                    </li>
+                    <li>
+                        <p>If 4 stations are owned</p>
+                        <p>{200}M</p>
+                    </li>
+                </ol>
+                <h4>mortgage Value 100M</h4>
+                <hr />
+                <br />
+                <h4>{args.cardCost}M</h4>
             </div>
         </div>
     );
